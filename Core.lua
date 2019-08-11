20aef82e6ae9be343af2a5e6ff9dd986a9368381
Jonathan Simpson <jsimpson.github@gmail.com>
2019-08-11 08:18:02 -0700

    fix autolure...

--- Core.lua
+++ Core.lua
@@ -126,10 +126,13 @@ button:RegisterForClicks("RightButtonUp")
 button:SetPoint("TOP", UIParent, "BOTTOM", 0, -5)
 
 button:SetScript("PreClick", function(self)
+    if UnitCastingInfo("player") then return end
+
     if config.autoArcaneLure and not BuffSearch("player", "Arcane Lure") then
         if GetItemCount(arcaneLureItemId) > 0 then
           self:SetAttribute("type", "item")
           self:SetAttribute("item", "item:"..arcaneLureItemId)
+          return
         end
     end
 
@@ -141,6 +144,7 @@ button:SetScript("PreClick", function(self)
                 self:SetAttribute("item", "item:"..lure)
                 self:SetAttribute("target-slot", INVSLOT_MAINHAND)
             end
+            return
         end
     end
 
